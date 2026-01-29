6c2f049d3d0e77e8ccd580b4ce31da938d25ee57
ellenoireQ <ketchupnaiso@gmail.com>
2026-01-29 11:24:13 +0700

    create CLI and says hello_world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+use crate::docs::hello_world::{self, hello_world::hello_world};
+mod docs;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    args: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.args.as_str() {
+        "hello_world" => {
+            hello_world();
+        }
+        _ => { //}
+        }
+    }
 }
